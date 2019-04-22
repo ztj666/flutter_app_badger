8f5c49f791cad0a46157b24e40cb3a281ce1588a
ztj <732811911@qq.com>
2019-04-22 15:55:03 +0800

    11

--- android/src/main/java/fr/g123k/flutterappbadger/FlutterAppBadgerPlugin.java
+++ android/src/main/java/fr/g123k/flutterappbadger/FlutterAppBadgerPlugin.java
@@ -1,6 +1,16 @@
 package fr.g123k.flutterappbadger;
 
+import android.annotation.TargetApi;
+import android.app.Application;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+
+import com.tot.badges.IconBadgeNumManager;
 
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
@@ -27,10 +37,43 @@ public class FlutterAppBadgerPlugin implements MethodCallHandler {
     final MethodChannel channel = new MethodChannel(registrar.messenger(), "g123k/flutter_app_badger");
     channel.setMethodCallHandler(new FlutterAppBadgerPlugin(registrar.activeContext()));
   }
-
+  @TargetApi(Build.VERSION_CODES.O)
+  private static NotificationChannel createNotificationChannel() {
+    String channelId = "test";
+    NotificationChannel channel = null;
+    channel = new NotificationChannel(channelId,
+            "Channel1", NotificationManager.IMPORTANCE_DEFAULT);
+    channel.enableLights(true); //是否在桌面icon右上角展示小红点
+    channel.setLightColor(Color.RED); //小红点颜色
+    channel.setShowBadge(true); //是否在久按桌面图标时显示此渠道的通知
+    return channel;
+  }
   @Override
   public void onMethodCall(MethodCall call, Result result) {
     if (call.method.equals("updateBadgeCount")) {
+      String notificationChannelId = null;
+      NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+      if (nm == null) return;
+      if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+        NotificationChannel notificationChannel = createNotificationChannel();
+        nm.createNotificationChannel(notificationChannel);
+        notificationChannelId = notificationChannel.getId();
+      }
+      Notification notification;
+      try {
+        notification = new NotificationCompat.Builder(context, notificationChannelId)
+                .setSmallIcon(context.getApplicationInfo().icon)
+                .setWhen(System.currentTimeMillis())
+                .setContentTitle("title")
+                .setContentText("您有一条新的消息！ ")
+                .setTicker("ticker")
+                .setAutoCancel(true)
+                .setNumber(1)
+                .build();
+        new IconBadgeNumManager().setIconBadgeNum((Application) context, notification, Integer.valueOf(call.argument("count").toString()));
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
       ShortcutBadger.applyCount(context, Integer.valueOf(call.argument("count").toString()));
       result.success(null);
     } else if (call.method.equals("removeBadge")) {
